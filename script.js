f7bec8206eb74c03ef2b9a598c23f5f18e7a62b4
Henrique <carloshenriquedesenvolvedor@gmail.com>
2023-10-17 05:56:40 -0300

    feature: function dark mode

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-changeThemeBtn.addEventListener("change", function() {
+//Toogle dark mode
+
+function toogleDarkMoode() {
     document.body.classList.toggle("dark")
-})
+}
+
+//Load dark an light mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+    if (darkMode) {
+        toogleDarkMoode()
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function() {
+    toogleDarkMoode();
+
+    //Save or remove dark mode
+    localStorage.removeItem('dark');
+
+    if(document.body.classList.contains('dark')) {
+        localStorage.setItem('dark', 1)
+    }
+
+});
